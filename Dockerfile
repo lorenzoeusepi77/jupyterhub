cfb12500e662bebf5f10c869dd98ad4bf5e99afa
lorenzoeusepi77 <lorenzo.eusepi77@gmail.com>
2017-09-28 10:21:13 +0200

    first commit

--- Dockerfile
+++ Dockerfile
@@ -10,24 +10,30 @@ RUN apt-get update && \
  && apt-get clean -y \
  && pip install psycopg2 
 
-#    pip install jupyterhub-ldapauthenticator
- 
 
 # ADD LDAP AUTH PACKAGE
 RUN pip install jupyterhub-ldapauthenticator
 RUN pip install jupyterhub-ldapcreateusers
 
-# ADD LOCAL USER
-RUN useradd -m -G shadow -p $(openssl passwd -1 rhea) rhea
-RUN chown rhea .
-RUN useradd -m -G shadow -p $(openssl passwd -1 lorenzo) lorenzo
-RUN chown lorenzo .
 
+# Install Spark Magic library
+RUN pip install sparkmagic
 
-RUN for name in io ganymede ; do useradd -m -p $(openssl passwd -1 $name) $name; done
+# Configure user home directory permission
+COPY /ubuntuconfig /etc
 
-# ADD Configurable-http-proxy
-#RUN npm install -g configurable-http-proxy
+# Init script spark magic
+ADD ./sparkmagic-init.sh /
+RUN chmod +x /sparkmagic-init.sh
+
+# ADD LOCAL ADMIN USER
+RUN useradd -m -G shadow -p $(openssl passwd -1 admin) admin
+RUN chown admin .
+
+#Configure sparkmagic json file
+COPY /sparkmagicconfig /etc/skel
+
+#Install notebook
 RUN pip install notebook
 
 #EXPOSE 8000
--- jupyterhubconfig/jupyterhub_config.py
+++ jupyterhubconfig/jupyterhub_config.py
@@ -6,6 +6,7 @@ import os
 
 c = get_config()
 
+
 # We rely on environment variables to configure JupyterHub so that we
 # avoid having to rebuild the JupyterHub container every time we change a
 # configuration parameter.
@@ -19,12 +20,15 @@ c = get_config()
 #c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
 
 #Add admin user
-#c.Authenticator.admin_users = {'lorenzo'}
-#c.Authenticator.admin_users = {'rhea','lorenzo'}
+c.Authenticator.admin_users = {'admin','lorenzo'}
+#c.Authenticator.whitelist = {'lorenzo', 'pippo'}
+c.JupyterHub.admin_access = True
+
 
 #Proxy authentication token
 c.JupyterHub.proxy_auth_token = '0bc02bede919e99a26de1e2a7a5aadfaf6228de836ec39a05a6c6942831d8fe5'
-#ConfigurableHTTPProxy.auth_token = '0bc02bede919e99a26de1e2a7a5aadfaf6228de836ec39a05a6c6942831d8fe5'
+
+c.JupyterHub.cookie_secret = bytes.fromhex('3a428b49012544f2d65c3d320f45a351b2e8dd80ac68ce27e45caf1b8971a894')
 
 # Set the log level by value or name.
 c.JupyterHub.log_level = 'DEBUG'
@@ -44,85 +48,12 @@ c.JupyterHub.db_url = 'postgresql://{user}:{password}@{host}/{db}'.format(
 
 
 
-# jupyterhub config file
-#
+# LDAP Config
 c.JupyterHub.authenticator_class = 'ldapcreateusers.LocalLDAPCreateUsers'
-#c.LocalLDAPCreateUsers.server_address = 'some.ldap.server'
-#c.LocalLDAPCreateUsers.server_port = 389
-#c.LocalLDAPCreateUsers.use_ssl = False
-#c.LocalLDAPCreateUsers.bind_dn_template = 'uid={username},dc=yourdomain,dc=com'
 c.LocalLDAPCreateUsers.create_system_users = True
 
-
-# Ldap configuration
-#Enable Ldap auth
-#c.JupyterHub.authenticator_class = 'ldapauthenticator.LDAPAuthenticator'
-
 #ldap Server config
-c.LDAPAuthenticator.server_address = '192.168.0.29'
+c.LDAPAuthenticator.server_address = 'server ip'
 c.LDAPAuthenticator.use_ssl = True
 c.LDAPAuthenticator.server_port = 636
-c.LDAPAuthenticator.bind_dn_template = 'uid={username},cn=users,cn=accounts,dc=daf,dc=gov,dc=it'
-#c.LDAPAuthenticator.lookup_dn = True
-#c.LDAPAuthenticator.lookup_dn_search_filter = '({uid}={login})'
-#c.LDAPAuthenticator.lookup_dn_search_user = 'cn=admins,cn=groups,cn=accounts,dc=daf,dc=gov,dc=it'
-#c.LDAPAuthenticator.lookup_dn_search_password = 'uWae6wae'
-#c.LDAPAuthenticator.user_search_base = 'cn=users,cn=accounts,dc=daf,dc=gov,dc=it'
-#c.LDAPAuthenticator.user_attribute = 'uid'
-#c.LDAPAuthenticator.lookup_dn_user_dn_attribute = 'cn'
-# GROUP
-#c.LDAPAuthenticator.allowed_groups = [
-#    'cn=jupyterhub_admin,cn=groups,cn=accounts,dc=daf,dc=gov,dc=it'
-#]
-#    'cn=researcher,ou=groups,dc=wikimedia,dc=org',
-#    'cn=operations,ou=groups,dc=wikimedia,dc=org'
-#]
-
-
-# LDAPAuthenticator.lookup_dn: Whether to try a reverse lookup to obtain the user's DN. Some LDAP servers, such as Active Directory, don't always bind with the true DN, so this allows us to discover it based on the username.
-#c.LDAPAuthenticator.lookup_dn = True
-
-#LDAPAuthenticator.user_search_base: Only used with lookup_dn=True. Defines the search base for looking up users in the directory.
-#c.LDAPAuthenticator.user_search_base = 'ou=People,dc=example,dc=com'
-
-#LDAPAuthenticator.user_attribute: Only used with lookup_dn=True. Defines the attribute that stores a user's username in your directory.
-# Active Directory
-#c.LDAPAuthenticator.user_attribute = 'sAMAccountName'
-
-# OpenLDAP
-#c.LDAPAuthenticator.user_attribute = 'uid'
-
-
-# in jupyterhub_config.py
-#from jupyterhub.spawner import LocalProcessSpawner
-
-#class SameUserSpawner(LocalProcessSpawner):
-#    """Local spawner that runs single-user servers as the same user as the Hub itself.
-#
-#    Overrides user-specific env setup with no-ops.
-#    """
-#    
-#    def make_preexec_fn(self, name):
-#        """no-op to avoid setuid"""
-#        return lambda : None
-#    
-#    def user_env(self, env):
-#        """no-op to avoid setting HOME dir, etc.""" 
-#        return env
-
-#c.JupyterHub.spawner_class = SameUserSpawner
-
-c.Authenticator.admin_users = {'rhea','pippo'}
-#c.Authenticator.whitelist = {'lorenzo', 'pippo'}
-
-c.JupyterHub.admin_access = True
-
-#c.LocalAuthenticator.create_system_users = True
-
-# jupyterhub config file
-
-
-# TLS config
-#c.JupyterHub.port = 443
-#c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-#c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+c.LDAPAuthenticator.bind_dn_template = 'uid={username},cn=users,cn=accounts,dc=test,dc=example,dc=com'
